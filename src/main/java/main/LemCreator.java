4210deb685c80131187c187b97354527e165adaf
voledoler <voledoler@yandex.ru>
2022-04-05 21:04:10 +0300

    Commit8: add total statistic metod

--- src/main/java/main/LemCreator.java
+++ src/main/java/main/LemCreator.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 public class LemCreator {
     public  HashMap<String, Integer> lemmas = new HashMap<>();
+
     public  String[] partsOfSpeech = {"СОЮЗ","МЕЖД","ПРЕДЛ","ЧАСТ"};
 
 
